ec6c4eb5a55293dac04a9f18c4c58abf9f21dc93
Yeralys Li Balmaseda <yli@ia.com.mx>
2026-02-20 10:27:05 -0300

    Refactor: Move MainActivity to ui package and remove boilerplate
    Description: To improve the project's structure and organization, MainActivity has been relocated to a new `ui` package. This separation prepares the codebase for future feature development by distinguishing the UI layer from other potential layers like data or domain.
    
    As part of this refactoring, the default boilerplate code, including the `Greeting` composable and its associated preview, has been removed. The `Scaffold` now contains an empty `Column` as a starting point for building the application's user interface.
    
    The `AndroidManifest.xml` has been updated to reflect the new path for `MainActivity`, and several unused imports and redundant attributes have been cleaned up.

--- app/src/main/java/com/lidev/mycountriesapp/ui/MainActivity.kt
+++ app/src/main/java/com/lidev/mycountriesapp/ui/MainActivity.kt
@@ -1,16 +1,14 @@
-package com.lidev.mycountriesapp
+package com.lidev.mycountriesapp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.lidev.mycountriesapp.ui.theme.MyCountriesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +18,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyCountriesAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Column(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyCountriesAppTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/lidev/mycountriesapp/ui/theme/Theme.kt
+++ app/src/main/java/com/lidev/mycountriesapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.lidev.mycountriesapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
