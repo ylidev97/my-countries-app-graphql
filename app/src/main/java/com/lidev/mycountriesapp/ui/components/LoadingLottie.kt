fd2c7959620ba61cc9bf260c9c9b5fa5ee5998f9
Yeralys Li Balmaseda <yli@ia.com.mx>
2026-02-21 02:50:12 -0300

    Chore: Remove unused imports in LoadingLottie
    Description: This commit removes unused import statements for `androidx.compose.foundation.background` and `androidx.compose.ui.graphics.Color` from the `LoadingLottie.kt` file. This is part of a code cleanup effort to improve code hygiene and maintainability.

--- app/src/main/java/com/lidev/mycountriesapp/ui/components/LoadingLottie.kt
+++ app/src/main/java/com/lidev/mycountriesapp/ui/components/LoadingLottie.kt
@@ -1,11 +1,9 @@
 package com.lidev.mycountriesapp.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
