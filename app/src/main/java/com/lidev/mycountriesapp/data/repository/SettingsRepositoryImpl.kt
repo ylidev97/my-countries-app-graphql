0718a77cfdc409c802dd59a3a08ad6e4354bd4bf
Yeralys Li Balmaseda <yli@ia.com.mx>
2026-03-02 00:47:51 -0300

    Feat: Clean up formatting in SettingsRepositoryImpl
    Description: Updated the SettingsRepositoryImpl class to improve code readability and maintainability. Added trailing commas to the constructor parameters and inserted empty lines between function overrides to follow consistent coding style guidelines. These changes do not affect functionality but ensure the codebase remains clean and structured.

--- app/src/main/java/com/lidev/mycountriesapp/data/repository/SettingsRepositoryImpl.kt
+++ app/src/main/java/com/lidev/mycountriesapp/data/repository/SettingsRepositoryImpl.kt
@@ -5,13 +5,15 @@ import com.lidev.mycountriesapp.domain.repository.SettingsRepository
 import kotlinx.coroutines.flow.Flow
 
 class SettingsRepositoryImpl(
-    private val settingsDataStore: SettingsDataStore
+    private val settingsDataStore: SettingsDataStore,
 ) : SettingsRepository {
     override val themeFlow: Flow<String> = settingsDataStore.themeFlow
     override val dynamicColorFlow: Flow<Boolean> = settingsDataStore.dynamicColorFlow
     override val languageFlow: Flow<String> = settingsDataStore.languageFlow
 
     override suspend fun setTheme(theme: String) = settingsDataStore.setTheme(theme)
+
     override suspend fun setDynamicColor(enabled: Boolean) = settingsDataStore.setDynamicColor(enabled)
+
     override suspend fun setLanguage(language: String) = settingsDataStore.setLanguage(language)
 }
